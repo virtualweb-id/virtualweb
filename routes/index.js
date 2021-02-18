51a5105b7ea8a9a574dc6f0b5701c77d2c5fa6c9
raflirach <rafli060395@gmail.com>
2021-02-19 01:02:07 +0700

    add authenticate

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router()
 const guest = require('./guest')
 const authRouter = require('./auth')
+const { authentication } = require('../middlewares/auth')
 
 router.use('/', authRouter)
+router.use(authentication)
 router.use('/guests', guest)
 
 module.exports = router
