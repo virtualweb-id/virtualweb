f5d0f4fe7467dc7afcd1e03fb04b1d9b87d0f850
raflirach <rafli060395@gmail.com>
2021-02-19 07:04:17 +0700

    add: wedding router

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 const router = require('express').Router()
-const guest = require('./guest')
+const guestRouter = require('./guest')
 const authRouter = require('./auth')
+const weddingRouter = require('./wedding')
 const { authentication } = require('../middlewares/auth')
 
 router.use('/', authRouter)
 router.use(authentication)
-router.use('/guests', guest)
+router.use('/guests', guestRouter)
+router.use('/weddings', weddingRouter)
 
 module.exports = router
