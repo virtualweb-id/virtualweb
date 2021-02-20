227fb552f1495c1007c601647739bc3e3e29623f
akira <akira@js-MacBook-Pro.local>
2021-02-20 23:36:03 +0700

    added cloudinary

---
+++ helpers/cloudinary.js
@@ -0,0 +1,8 @@
+const cloudinary = require('cloudinary').v2
+cloudinary.config({
+  cloud_name: 'dbgodqvrl',
+  api_key: '658291717315763',
+  api_secret: 'H33si7bxYI11fWnbIGfzluKNSuk'
+})
+
+module.exports = cloudinary
